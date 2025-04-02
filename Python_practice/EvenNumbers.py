a51cd17abe0a4edf70bf5fba0cefc48f42913daf
Initiate-Now <initiatenow21@gmail.com>
2025-04-02 19:32:33 +0530

    Even Number

---
+++ Python_practice/EvenNumbers.py
@@ -0,0 +1,13 @@
+'''*****************************************************
+* Name     : EvenNumbers.py
+* Category : Control Flow - Loops(for, while) and Loop Control 
+             Statements (break, continue).
+* Program  : Write a Python script that prints all even numbers
+             between 1 and 20 using a while loop.
+* Author   : Navin Chakravarthy Kamalakannan
+*********************************************************'''
+num = 2 # start from first even number
+
+while num<=20:
+    print(num)
+    num += 2 # Increament by 2 to get the next even number
