9273908a5eda117aeff4e068066d37418c6df0a9
Initiate-Now <initiatenow21@gmail.com>
2025-04-06 16:52:52 +0530

    Friend Age Dictionary

---
+++ Python_practice/FriendAgeDict.py
@@ -0,0 +1,18 @@
+'''*****************************************************
+* Name     : FriendAgeDict.py
+* Category : Dictionaries - Creating, Accessing, Modifying, and
+             Common Dictionary Methods.
+* Program  : Write a Python dictionary to store the names 
+             and ages of three friends. Print the name of 
+             one of your friends using their name as the key.
+* Author   : Navin Chakravarthy Kamalakannan
+*********************************************************'''
+# Dictionary to store names and ages
+friends = {
+    "Navin": 25,
+    "Loga": 24,
+    "Lalith": 26
+}
+
+# Print the age of one friend
+print("Loga's age is:", friends["Loga"])
